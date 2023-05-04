fc10123a489312b3ca0deed94c7cb56d62865647
iqbalroni <roniesaputra92@gmail.com>
2023-05-04 16:44:02 +0700

    view layout

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,46 @@
+<!--   Core JS Files   -->
+<script src="{{ asset('template') }}/assets/js/core/jquery.3.2.1.min.js"></script>
+<script src="{{ asset('template') }}/assets/js/core/popper.min.js"></script>
+<script src="{{ asset('template') }}/assets/js/core/bootstrap.min.js"></script>
+<!-- jQuery UI -->
+<script src="{{ asset('template') }}/assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
+<script src="{{ asset('template') }}/assets/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js"></script>
+<!-- Bootstrap Toggle -->
+<script src="{{ asset('template') }}/assets/js/plugin/bootstrap-toggle/bootstrap-toggle.min.js"></script>
+<!-- jQuery Scrollbar -->
+<script src="{{ asset('template') }}/assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script>
+<!-- Atlantis JS -->
+<script src="{{ asset('template') }}/assets/js/atlantis.min.js"></script>
+<!-- Atlantis DEMO methods, don't include it in your project! -->
+<script src="{{ asset('template') }}/assets/js/setting-demo2.js"></script>
+<script>
+    $('#displayNotif').on('click', function() {
+        var placementFrom = $('#notify_placement_from option:selected').val();
+        var placementAlign = $('#notify_placement_align option:selected').val();
+        var state = $('#notify_state option:selected').val();
+        var style = $('#notify_style option:selected').val();
+        var content = {};
+
+        content.message = 'Turning standard Bootstrap alerts into "notify" like notifications';
+        content.title = 'Bootstrap notify';
+        if (style == "withicon") {
+            content.icon = 'fa fa-bell';
+        } else {
+            content.icon = 'none';
+        }
+        content.url = 'index.html';
+        content.target = '_blank';
+
+        $.notify(content, {
+            type: state,
+            placement: {
+                from: placementFrom,
+                align: placementAlign
+            },
+            time: 1000,
+        });
+    });
+</script>
+</body>
+
+</html>
---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,144 @@
+@include('layout.head')
+<div class="wrapper">
+    @include('components.main-header')
+    <!-- Sidebar -->
+    @include('components.sidebar')
+    <div class="main-panel">
+        @yield('content')
+
+
+        {{-- notif --}}
+        <div class="quick-sidebar">
+            <a href="#" class="close-quick-sidebar">
+                <i class="flaticon-cross"></i>
+            </a>
+            <div class="quick-sidebar-wrapper">
+                <ul class="nav nav-tabs nav-line nav-color-secondary" role="tablist">
+                    <li class="nav-item"> <a class="nav-link active show" data-toggle="tab" href="#messages"
+                            role="tab" aria-selected="true">Messages</a> </li>
+                </ul>
+                <div class="tab-content mt-3">
+                    <div class="tab-chat tab-pane fade show active" id="messages" role="tabpanel">
+                        <div class="messages-contact">
+                            <div class="quick-wrapper">
+                                <div class="quick-scroll scrollbar-outer">
+                                    <div class="quick-content contact-content">
+                                        <span class="category-title mt-0">Contacts</span>
+                                        <div class="avatar-group">
+                                            <div class="avatar">
+                                                <img src="{{ asset('template') }}/assets/img/jm_denis.jpg"
+                                                    alt="..."
+                                                    class="avatar-img rounded-circle border border-white">
+                                            </div>
+                                            <div class="avatar">
+                                                <img src="{{ asset('template') }}/assets/img/chadengle.jpg"
+                                                    alt="..."
+                                                    class="avatar-img rounded-circle border border-white">
+                                            </div>
+                                            <div class="avatar">
+                                                <img src="{{ asset('template') }}/assets/img/mlane.jpg" alt="..."
+                                                    class="avatar-img rounded-circle border border-white">
+                                            </div>
+                                            <div class="avatar">
+                                                <img src="{{ asset('template') }}/assets/img/talha.jpg" alt="..."
+                                                    class="avatar-img rounded-circle border border-white">
+                                            </div>
+                                            <div class="avatar">
+                                                <span class="avatar-title rounded-circle border border-white">+</span>
+                                            </div>
+                                        </div>
+                                        <span class="category-title">Recent</span>
+                                        <div class="contact-list contact-list-recent">
+                                            <div class="user">
+                                                <a href="#">
+                                                    <div class="avatar avatar-online">
+                                                        <img src="{{ asset('template') }}/assets/img/jm_denis.jpg"
+                                                            alt="..."
+                                                            class="avatar-img rounded-circle border border-white">
+                                                    </div>
+                                                    <div class="user-data">
+                                                        <span class="name">Jimmy Denis</span>
+                                                        <span class="message">How are you ?</span>
+                                                    </div>
+                                                </a>
+                                            </div>
+                                            <div class="user">
+                                                <a href="#">
+                                                    <div class="avatar avatar-offline">
+                                                        <img src="{{ asset('template') }}/assets/img/chadengle.jpg"
+                                                            alt="..."
+                                                            class="avatar-img rounded-circle border border-white">
+                                                    </div>
+                                                    <div class="user-data">
+                                                        <span class="name">Chad</span>
+                                                        <span class="message">Ok, Thanks !</span>
+                                                    </div>
+                                                </a>
+                                            </div>
+                                            <div class="user">
+                                                <a href="#">
+                                                    <div class="avatar avatar-offline">
+                                                        <img src="{{ asset('template') }}/assets/img/mlane.jpg"
+                                                            alt="..."
+                                                            class="avatar-img rounded-circle border border-white">
+                                                    </div>
+                                                    <div class="user-data">
+                                                        <span class="name">John Doe</span>
+                                                        <span class="message">Ready for the meeting today
+                                                            with...</span>
+                                                    </div>
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <span class="category-title">Other Contacts</span>
+                                        <div class="contact-list">
+                                            <div class="user">
+                                                <a href="#">
+                                                    <div class="avatar avatar-online">
+                                                        <img src="{{ asset('template') }}/assets/img/jm_denis.jpg"
+                                                            alt="..."
+                                                            class="avatar-img rounded-circle border border-white">
+                                                    </div>
+                                                    <div class="user-data2">
+                                                        <span class="name">Jimmy Denis</span>
+                                                        <span class="status">Online</span>
+                                                    </div>
+                                                </a>
+                                            </div>
+                                            <div class="user">
+                                                <a href="#">
+                                                    <div class="avatar avatar-offline">
+                                                        <img src="{{ asset('template') }}/assets/img/chadengle.jpg"
+                                                            alt="..."
+                                                            class="avatar-img rounded-circle border border-white">
+                                                    </div>
+                                                    <div class="user-data2">
+                                                        <span class="name">Chad</span>
+                                                        <span class="status">Active 2h ago</span>
+                                                    </div>
+                                                </a>
+                                            </div>
+                                            <div class="user">
+                                                <a href="#">
+                                                    <div class="avatar avatar-away">
+                                                        <img src="{{ asset('template') }}/assets/img/talha.jpg"
+                                                            alt="..."
+                                                            class="avatar-img rounded-circle border border-white">
+                                                    </div>
+                                                    <div class="user-data2">
+                                                        <span class="name">Talha</span>
+                                                        <span class="status">Away</span>
+                                                    </div>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @include('layout.footer')
