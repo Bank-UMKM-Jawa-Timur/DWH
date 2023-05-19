c55f509c20518518f6cee0dc3e88a0bfa55513b8
oong <37205723+oong26@users.noreply.github.com>
2023-05-19 16:01:58 +0700

    feat/notificatoin : retrieve notification

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -6,10 +6,45 @@ use App\Models\Notification;
 use App\Models\NotificationTemplate;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class NotificationController extends Controller
 {
+    public function index()
+    {
+        try {
+            $param['title'] = 'Notifikasi';
+            $param['pageTitle'] = 'Notifikasi';
+
+            $param['data'] = Notification::select(
+                                    'notifications.id',
+                                    'notifications.user_id',
+                                    'notifications.read',
+                                    'notifications.created_at',
+                                    'notifications.updated_at',
+                                    'nt.title',
+                                    'nt.content',
+                                    'nt.action_id',
+                                    'nt.role_id',
+                                )
+                                ->join('notification_templates AS nt', 'nt.id', 'notifications.template_id')
+                                ->where('notifications.user_id', Auth::user()->id)
+                                ->orderBy('notifications.created_at', 'DESC')
+                                ->get();
+            $param['total_belum_dibaca'] = Notification::select('notifications.id')
+                                            ->join('users AS u', 'u.id', 'notifications.user_id')
+                                            ->where('u.id', Auth::user()->id)
+                                            ->count();
+
+            return view('pages.notifikasi.index', $param);
+        } catch (\Exception $e) {
+            return back()->withError('Terjadi kesalahan');
+        } catch (\Illuminate\Database\QueryException $e) {
+            return back()->withError('Terjadi kesalahan pada database');
+        }
+    }
+
     public function send($action_id)
     {
         try {
