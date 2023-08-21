29384bc8a5c1b1684771288aa7f88c6613aaf964
oong26 <mkhalil26122000@gmail.com>
2023-08-21 17:20:05 +0700

    develop : adding int parse to ftp_timeout

--- config/filesystems.php
+++ config/filesystems.php
@@ -61,7 +61,7 @@ return [
             'port' => (int) env('FTP_PORT', 21),
             'passive' => false,
             'ignorePassiveAddress' => true, 
-            'timeout' => env('FTP_TIMEOUT', 360),
+            'timeout' => (int) env('FTP_TIMEOUT', 360),
             'ssl'      => env('FTP_SSL', false),
         ]
 
