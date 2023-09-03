2c84727d28ec4030b7be3d872b7981058da147ee
sahrullah <mohammadsahrullah894@gmail.com>
2023-09-03 13:37:42 +0700

    redesign: adding modal imbal-jasa & template-notifikasi

--- resources/views/pages/imbal_jasa/index.blade.php
+++ resources/views/pages/imbal_jasa/index.blade.php
@@ -2,162 +2,170 @@
 
 @section('title', $title)
 
+
+@section('modal')
+    <!-- Modal-tambah -->
+    @include('pages.imbal_jasa.modal.create')
+    <!-- Modal-edit -->
+    @include('pages.imbal_jasa.modal.edit')
+@endsection
+
 @section('content')
 
     <div class="head-pages">
-      <p class="text-sm">Master</p>
-      <h2 class="text-2xl font-bold text-theme-primary tracking-tighter">
-          {{ $pageTitle }}
-      </h2>
+        <p class="text-sm">Master</p>
+        <h2 class="text-2xl font-bold text-theme-primary tracking-tighter">
+            {{ $pageTitle }}
+        </h2>
     </div>
     <div class="body-pages">
-      <div class="table-wrapper bg-white border rounded-md w-full p-2">
-          <div class="table-accessiblity lg:flex text-center lg:space-y-0 space-y-5 justify-between">
-              <div class="title-table lg:p-3 p-2 text-center">
-                  <h2 class="font-bold text-lg text-theme-text tracking-tighter">
-                    {{ $pageTitle }}
-                  </h2>
-              </div>
-              <div class="table-action flex lg:justify-normal justify-center p-2 gap-2">
-                  <button id="form-toggle" class="px-6 py-2 bg-theme-primary flex gap-3 rounded text-white">
-                      <span class="lg:mt-0 mt-0">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24">
-                              <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                  stroke-width="2" d="M5 12h14m-7-7v14" />
-                          </svg>
-                      </span>
-                      <span class="lg:block hidden"> Tambah Imbal jasa </span>
-                  </button>
-              </div>
-          </div>
-          <div class="lg:flex lg:space-y-0 space-y-5 lg:text-left text-center justify-between mt-2 p-2">
-              <div class="sorty pl-1 w-full">
-                  <label for="" class="mr-3 text-sm text-neutral-400">show</label>
-                  <select name="" class="border px-4 py-1.5 cursor-pointer rounded appearance-none text-center"
-                      id="">
-                      <option value="">5</option>
-                      <option value="">10</option>
-                      <option value="">15</option>
-                      <option value="">20</option>
-                  </select>
-                  <label for="" class="ml-3 text-sm text-neutral-400">entries</label>
-              </div>
-              <div class="search-table lg:w-96 w-full">
-                  <div class="input-search text-[#BFBFBF] rounded-md border flex gap-2">
-                      <span class="mt-2 ml-3">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24">
-                              <path fill="currentColor"
-                                  d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5A6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5S14 7.01 14 9.5S11.99 14 9.5 14z" />
-                          </svg>
-                      </span>
-                      <input type="search" placeholder="Search" class="p-2 rounded-md w-full outline-none text-[#BFBFBF]"
-                          autocomplete="off" />
-                  </div>
-              </div>
-          </div>
-          <div class="tables mt-2">
-              <table class="table-imbal-jasa table-auto w-full">
-                  <thead>
-                      <tr>
-                          <th rowspan="2" scope="col">
-                              No.
-                          </th>
-                          <th rowspan="2" scope="col">
-                              Plafond
-                          </th>
-                          <th scope="col" colspan="3">
-                              Imbal jasa
-                          </th>
-                          <th rowspan="2" scope="col">
-                              Aksi
-                          </th>
-                      </tr>
-                      <tr>
-                          <th>12</th>
-                          <th>24</th>
-                          <th>36 s/d 60</th>
-                      </tr>
-                  </thead>
-                  <tbody>
-                      <tr>
-                          <td>1</td>
-                          <td>
-                              < 10.000.000</td>
-                          <td>100.000</td>
-                          <td>200.000</td>
-                          <td>300.000</td>
-                          <td>
-                              <div class="dropdown">
-                                  <button class="px-4 py-2 bg-theme-btn/10 rounded text-theme-btn">
-                                      Selangkapnya
-                                  </button>
-                                  <ul class="dropdown-menu">
-                                      <li class="">
-                                          <a class="item-dropdown toggle-form-edit" data-form-id="form-1"
-                                              href="#">Edit</a>
-                                      </li>
-                                      <li class="">
-                                          <a class="item-dropdown" href="#">Hapus</a>
-                                      </li>
-                                  </ul>
-                              </div>
-                          </td>
-                      </tr>
-                      <tr>
-                          <td>2</td>
-                          <td>10.000.000 s/d 15.000.000</td>
-                          <td>150.000</td>
-                          <td>300.000</td>
-                          <td>400.000</td>
-                          <td>
-                              <div class="dropdown">
-                                  <button class="px-4 py-2 bg-theme-btn/10 rounded text-theme-btn">
-                                      Selangkapnya
-                                  </button>
-                                  <ul class="dropdown-menu">
-                                      <li class="">
-                                          <a class="item-dropdown toggle-form-edit" data-form-id="form-2"
-                                              href="#">Edit</a>
-                                      </li>
-                                      <li class="">
-                                          <a class="item-dropdown" href="#">Hapus</a>
-                                      </li>
-                                  </ul>
-                              </div>
-                          </td>
-                      </tr>
-                  </tbody>
-              </table>
-          </div>
-          <div class="footer-table p-3 text-theme-text lg:flex lg:space-y-0 space-y-10 justify-between">
-              <div>
-                  <p class="mt-3 text-sm">Menampilkan 1 - 5 dari 100 Data</p>
-              </div>
-              <div>
-                  <div class="pagination">
-                      <button class="btn-pagination">Previous</button>
-                      <button class="btn-pagination is-active">1</button>
-                      <button class="btn-pagination">2</button>
-                      <button class="btn-pagination">3</button>
-                      <button class="btn-pagination">4</button>
-                      <button class="btn-pagination">5</button>
-                      <button class="btn-pagination">...</button>
-                      <button class="btn-pagination">100</button>
-                      <button class="btn-pagination">Next</button>
-                  </div>
-              </div>
-          </div>
-      </div>
-  </div>
+        <div class="table-wrapper bg-white border rounded-md w-full p-2">
+            <div class="table-accessiblity lg:flex text-center lg:space-y-0 space-y-5 justify-between">
+                <div class="title-table lg:p-3 p-2 text-center">
+                    <h2 class="font-bold text-lg text-theme-text tracking-tighter">
+                        {{ $pageTitle }}
+                    </h2>
+                </div>
+                <div class="table-action flex lg:justify-normal justify-center p-2 gap-2">
+                    <button data-target-id="add-imbal-jasa" class="toggle-modal px-6 py-2 bg-theme-primary flex gap-3 rounded text-white">
+                        <span class="lg:mt-0 mt-0">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24">
+                                <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="M5 12h14m-7-7v14" />
+                            </svg>
+                        </span>
+                        <span class="lg:block hidden"> Tambah Imbal jasa </span>
+                    </button>
+                </div>
+            </div>
+            <div class="lg:flex lg:space-y-0 space-y-5 lg:text-left text-center justify-between mt-2 p-2">
+                <div class="sorty pl-1 w-full">
+                    <label for="" class="mr-3 text-sm text-neutral-400">show</label>
+                    <select name="" class="border px-4 py-1.5 cursor-pointer rounded appearance-none text-center"
+                        id="">
+                        <option value="">5</option>
+                        <option value="">10</option>
+                        <option value="">15</option>
+                        <option value="">20</option>
+                    </select>
+                    <label for="" class="ml-3 text-sm text-neutral-400">entries</label>
+                </div>
+                <div class="search-table lg:w-96 w-full">
+                    <div class="input-search text-[#BFBFBF] rounded-md border flex gap-2">
+                        <span class="mt-2 ml-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24">
+                                <path fill="currentColor"
+                                    d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5A6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5S14 7.01 14 9.5S11.99 14 9.5 14z" />
+                            </svg>
+                        </span>
+                        <input type="search" placeholder="Search" class="p-2 rounded-md w-full outline-none text-[#BFBFBF]"
+                            autocomplete="off" />
+                    </div>
+                </div>
+            </div>
+            <div class="tables mt-2">
+                <table class="table-imbal-jasa table-auto w-full">
+                    <thead>
+                        <tr>
+                            <th rowspan="2" scope="col">
+                                No.
+                            </th>
+                            <th rowspan="2" scope="col">
+                                Plafond
+                            </th>
+                            <th scope="col" colspan="3">
+                                Imbal jasa
+                            </th>
+                            <th rowspan="2" scope="col">
+                                Aksi
+                            </th>
+                        </tr>
+                        <tr>
+                            <th>12</th>
+                            <th>24</th>
+                            <th>36 s/d 60</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>
+                                < 10.000.000</td>
+                            <td>100.000</td>
+                            <td>200.000</td>
+                            <td>300.000</td>
+                            <td>
+                                <div class="dropdown">
+                                    <button class="px-4 py-2 bg-theme-btn/10 rounded text-theme-btn">
+                                        Selangkapnya
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                        <li class="">
+                                            <a class="item-dropdown toggle-modal" data-target-id="edit-imbal-jasa"
+                                                href="#">Edit</a>
+                                        </li>
+                                        <li class="">
+                                            <a class="item-dropdown" href="#">Hapus</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>10.000.000 s/d 15.000.000</td>
+                            <td>150.000</td>
+                            <td>300.000</td>
+                            <td>400.000</td>
+                            <td>
+                                <div class="dropdown">
+                                    <button class="px-4 py-2 bg-theme-btn/10 rounded text-theme-btn">
+                                        Selangkapnya
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                        <li class="">
+                                            <a class="item-dropdown toggle-modal" data-target-id="edit-imbal-jasa"
+                                                href="#">Edit</a>
+                                        </li>
+                                        <li class="">
+                                            <a class="item-dropdown" href="#">Hapus</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="footer-table p-3 text-theme-text lg:flex lg:space-y-0 space-y-10 justify-between">
+                <div>
+                    <p class="mt-3 text-sm">Menampilkan 1 - 5 dari 100 Data</p>
+                </div>
+                <div>
+                    <div class="pagination">
+                        <button class="btn-pagination">Previous</button>
+                        <button class="btn-pagination is-active">1</button>
+                        <button class="btn-pagination">2</button>
+                        <button class="btn-pagination">3</button>
+                        <button class="btn-pagination">4</button>
+                        <button class="btn-pagination">5</button>
+                        <button class="btn-pagination">...</button>
+                        <button class="btn-pagination">100</button>
+                        <button class="btn-pagination">Next</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <!-- Modal-tambah -->
-    
+
 
     <!-- Modal-edit -->
-   
+
 
     {{-- Modal Delete --}}
-    
+
 
     @push('extraScript')
         <script src="{{ asset('template') }}/assets/js/plugin/datatables/datatables.min.js"></script>
