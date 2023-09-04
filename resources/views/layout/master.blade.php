1086825e63def28ccc19b2e1509bfdd4f0c99453
oong26 <mkhalil26122000@gmail.com>
2023-09-04 12:40:12 +0700

    develop : fix html head title

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -6,7 +6,7 @@
       name="viewport"
       content="width=device-width, initial-scale=1.0"
     />
-    <title>Dashboard - Superadmin</title>
+    <title>Dashboard KKB</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
