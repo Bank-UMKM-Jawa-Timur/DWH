d2147b27936d50d0a419f8ec788bf0df0d1c3d8e
sahrullah <mohammadsahrullah894@gmail.com>
2023-10-11 16:54:22 +0700

    develop: fix tab table

--- resources/views/pages/kredit/index.blade.php
+++ resources/views/pages/kredit/index.blade.php
@@ -858,7 +858,6 @@
                 </div>
             </div>
         </div>
-    </div>
     @if (\Session::get(config('global.role_id_session')) != 3)
         <div id="tab-import-kkb" class="tab-content-table">
             <div class="table-wrapper bg-white border rounded-md w-full p-2">
@@ -948,7 +947,7 @@
         </div>
     @endif
     </div>
-
+</div>
 @endsection
 
 @push('extraScript')
