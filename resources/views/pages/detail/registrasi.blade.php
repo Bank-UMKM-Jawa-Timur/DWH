3f1e1f57de7bd24b250cbda2dedff6ed8ea7664b
oong <37205723+oong26@users.noreply.github.com>
2023-11-08 14:16:55 +0700

    feat/asuransi-dashboard : fix pie chart registrasi

--- resources/views/pages/detail/registrasi.blade.php
+++ resources/views/pages/detail/registrasi.blade.php
@@ -130,8 +130,8 @@
 
         // chart Registrasi
         var optionsRegistrasi = {
-            labels: ['Registrasi', 'Belum Registrasi'],
-            series: [registered, belum_registrasi],
+            labels: ['Sudah', 'Belum'],
+            series: [(registered + not_registered), belum_registrasi],
             chart: {
                 type: 'donut',
                 width: '100%',
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -880,8 +880,8 @@
 
      // chart Registrasi
     var optionsRegistrasi = {
-        labels: ['Registrasi', 'Belum Registrasi'],
-        series: [registered, belum_registrasi],
+        labels: ['Sudah', 'Belum'],
+        series: [(registered + not_registered), belum_registrasi],
         chart: {
             type: 'donut',
             width: '100%',
